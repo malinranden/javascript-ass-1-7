9bc62ac97c197d74b5e750e3935fcfc0009cc46f
Malin Randén <rantatalo94@gmail.com>
2025-10-13 14:21:33 +0200

    first and last commit

---
+++ script/index.js
@@ -0,0 +1,35 @@
+console.log("Hello World!");
+// alert("this message comes up in a pop up window, but not in the console");
+// prompt("this is an other");
+
+
+let Name = "Malin";
+let lastName = "Randén";
+let age = 31;
+let favoriteColor = "purple";
+// console.log(age);
+
+console.log(`My name is ${Name} ${lastName} and My age is ${age}.
+My favorite color is ${favoriteColor}!`);
+console.log(`And my age is ${age}.`)
+
+// let x = 5;
+// let y = "5";
+// console.log(typeof x);
+// console.log(typeof y);
+
+
+// let guitarStrings = 6;
+// guitarStrings = guitarStrings + 1;
+// console.log(guitarStrings); // nytt värde av 7
+
+
+let playingCards = 52;
+let tarotCards = 78;
+
+console.log(playingCards + tarotCards)
+console.log(tarotCards - playingCards)
+console.log(tarotCards * 10);
+console.log(playingCards / 2);
+console.log(playingCards % 3);
+console.log(tarotCards += 1);
