8039a023d50cccf671a40cf342c31693db719ae9
Malin Randén <rantatalo94@gmail.com>
2025-10-15 13:51:14 +0200

    changes working ass 2

--- script/assignment2.js
+++ script/assignment2.js
@@ -1,10 +1,7 @@
-// let pricetag = ("Enter pricetag", "$39.99");
-// prompt(pricetag); 
+// // let pricetag = ("Enter pricetag", "$39.99");
+// // prompt(pricetag); 
 
-let pricetag = prompt("Enter pricetag")
-
-
-let cost = "$39.99";
+let cost = prompt("Enter pricetag");
 console.log(cost);
 
 cost = cost.slice(1);
@@ -18,30 +15,3 @@ console.log(cost);
 
 cost = "your new price is $" + cost;
 console.log(cost);
-
-
-
-
-
-// let cost = "$39.99";
-// console.log(cost);
-
-// cost = cost.slice(1);
-// console.log(cost);
-
-// cost *= 0.9; 
-// console.log(cost);
-
-// cost = "your new price is $" + cost;
-// console.log(cost);
-
-
-
-// let price = cost.slice(1);
-// console.log(price);
-
-// price *= 0.9; 
-// console.log(price);
-
-// let reducedPrice = "$35.991";
-// console.log(`your new price is ` + reducedPrice);
