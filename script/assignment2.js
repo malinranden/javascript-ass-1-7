c8aa3ab201fe918466ea2a94150061ba3674a29c
Malin Randén <rantatalo94@gmail.com>
2025-10-15 12:18:03 +0200

    test ass 2

---
+++ script/assignment2.js
@@ -0,0 +1,47 @@
+// let pricetag = ("Enter pricetag", "$39.99");
+// prompt(pricetag); 
+
+let pricetag = prompt("Enter pricetag")
+
+
+let cost = "$39.99";
+console.log(cost);
+
+cost = cost.slice(1);
+console.log(cost);
+
+cost *= 0.9; 
+console.log(cost);
+
+cost = parseInt(cost);
+console.log(cost);
+
+cost = "your new price is $" + cost;
+console.log(cost);
+
+
+
+
+
+// let cost = "$39.99";
+// console.log(cost);
+
+// cost = cost.slice(1);
+// console.log(cost);
+
+// cost *= 0.9; 
+// console.log(cost);
+
+// cost = "your new price is $" + cost;
+// console.log(cost);
+
+
+
+// let price = cost.slice(1);
+// console.log(price);
+
+// price *= 0.9; 
+// console.log(price);
+
+// let reducedPrice = "$35.991";
+// console.log(`your new price is ` + reducedPrice);
