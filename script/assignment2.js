1a5a6a276f2a8f58f821e6b04b8b87a8340cf0a6
Malin Randén <rantatalo94@gmail.com>
2025-10-16 08:12:17 +0200

    update after feedback on ass 2

--- script/assignment2.js
+++ script/assignment2.js
@@ -1,6 +1,8 @@
 // // let pricetag = ("Enter pricetag", "$39.99");
 // // prompt(pricetag); 
 
+// This is my version. See below for Michelles advice
+
 let cost = prompt("Enter pricetag");
 console.log(cost);
 
@@ -15,3 +17,20 @@ console.log(cost);
 
 cost = "your new price is $" + cost;
 console.log(cost);
+
+
+// Advice 
+// Use new variables for the new output 
+// and use backticks 
+
+let price = prompt(`Enter pricetag`);
+console.log(price);
+
+let input = parseFloat(price.slice(1));
+console.log(input);
+
+let discount = input * 0.9;
+console.log(discount);
+
+console.log(`Your new discounted price is $${discount}`);
+console.log(`And the original price was ${price}`);
