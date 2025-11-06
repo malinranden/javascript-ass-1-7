9f910ab2780fd2dedabdde57156d2ee4feb4ef99
Malin Randén <rantatalo94@gmail.com>
2025-11-06 14:30:57 +0100

    assignment 5

--- script/assignment5.js
+++ script/assignment5.js
@@ -1,32 +1,42 @@
 // BANK ACCOUNT
-// allt funkar fram till getBalance delen ( utan return ). Efter det vet jag inte
+//
 
 const account = {
     accountName: "Malin Randén",
     balance: 10000,
-    addedMoney: 500,
-    takenMoney: 200,
     getBalance() {
+        alert(`Your balance is ${this.balance}.`)
         console.log(account.balance);
     },
     deposit() {
-        let deposit = this.balance + this.addedMoney;
-        console.log(deposit);
+        let deposit = prompt(`How much do you want to deposit?`);
+        balance = this.balance + parseFloat(deposit);
+        console.log(balance);
+        alert(`Your new balance is ${balance}.`);
     },
     withdrawal() {
-        let withdrawal = this.balance - this.takenMoney;
-        console.log(withdrawal);
+        let withdrawal = prompt(`How much do you want to withdraw?`);
+        balance = balance - parseFloat(withdrawal); 
+        // return withdrawal;
+        if (withdrawal > balance) {
+            alert(`You do not have enough balance on your account.`)
+        } else {
+            console.log(balance);
+        }
+        // console.log(balance);
     },
     getAccountName() {
         console.log(`This account belongs to ${this.accountName}.`)
     },
-    accountError() {
-        if (this.balance < 0)
-            console.log(`Error`);
-    }
 }
 account.getBalance();
 account.deposit();
 account.withdrawal();
 account.getAccountName();
-account.accountError();
+// account.accountError();
+
+// witdrawal 
+// if/else
+//     om mängden är högre än mängden på kontot - ge en console.log på inte tillräckligt med pengar
+//     om det är tillräckligt - else statement = uträkning
+//     Kan lägga den i witdrawal function men kan ha egen function (accountError)
