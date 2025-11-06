afb0834aae3cd88c09938f834f9acb13ec70276f
Malin Randén <rantatalo94@gmail.com>
2025-11-06 16:23:36 +0100

    starting code from michelle assignment 6

---
+++ script/assignment6.js
@@ -0,0 +1,29 @@
+// let running
+while () {
+    const choice = prompt(`
+        Book Tracker
+        1. Add Books
+        2. List Books
+        3. Mark Book as Read
+        4. Exit
+        Enter your choice:`);
+}
+
+switch (choice) {
+    case "1":
+        addBook();
+        break;
+    case "2":
+        listBooks();
+        break;
+    case "3":
+        const title = prompt(`Enter the title of the book to mark as read:`);
+        markAsRead(title);
+        break;
+    case "4":
+        running = false;
+        alert(`Goodbye!`);
+        break;
+    default:
+        alert(`Invalid choice.`);
+}
